d25c1ff34a44b69a1cbc2c309e13169e8da4ec0a
Bhagyalakshmi1983 <lakshmithota04@gmail.com>
2022-10-11 19:01:02 +0530

    Created TopHeader and Header Section

---
+++ components/layout/TopHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TopHeader() {
+  return (
+    <div className="bg-yellow-200">
+      <h1 className="cursor-pointer hover:text-purple-400 text-center">
+        KOOVS IS BACK! NEWER & BETTER</h1>
+    </div>
+  )
+}
